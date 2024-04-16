6e2de134f2b4e5584d22b27dea0dafea7aec930b
lstung219 <lstung2004@gmail.com>
2024-04-16 15:54:36 +0700

    test

--- QLThuVien/Login.Designer.cs
+++ QLThuVien/Login.Designer.cs
@@ -28,151 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btexit = new System.Windows.Forms.Button();
-            this.btlogin = new System.Windows.Forms.Button();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.textBoxusname = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            panel1 = new Panel();
+            label3 = new Label();
+            btexit = new Button();
+            btlogin = new Button();
+            panel3 = new Panel();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            panel2 = new Panel();
+            textBoxusname = new TextBox();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.btexit);
-            this.panel1.Controls.Add(this.btlogin);
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(970, 558);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            panel1.BackColor = Color.Tomato;
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(btexit);
+            panel1.Controls.Add(btlogin);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            panel1.Location = new Point(15, 19);
+            panel1.Margin = new Padding(4, 5, 4, 5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1212, 872);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(128, 35);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(735, 44);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Chào Mừng Bạn Đến Với Thư Viện Điện Tử";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(64, 64, 64);
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Font = new Font("Microsoft Sans Serif", 22F);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(160, 55);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(856, 54);
+            label3.TabIndex = 1;
+            label3.Text = "Chào Mừng Bạn Đến Với Thư Viện Điện Tử";
+            label3.Click += label3_Click;
             // 
             // btexit
             // 
-            this.btexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btexit.ForeColor = System.Drawing.Color.Black;
-            this.btexit.Location = new System.Drawing.Point(651, 380);
-            this.btexit.Name = "btexit";
-            this.btexit.Size = new System.Drawing.Size(157, 33);
-            this.btexit.TabIndex = 4;
-            this.btexit.Text = "Thoát";
-            this.btexit.UseVisualStyleBackColor = true;
-            this.btexit.Click += new System.EventHandler(this.btexit_Click);
+            btexit.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            btexit.ForeColor = Color.Black;
+            btexit.Location = new Point(814, 594);
+            btexit.Margin = new Padding(4, 5, 4, 5);
+            btexit.Name = "btexit";
+            btexit.Size = new Size(196, 52);
+            btexit.TabIndex = 4;
+            btexit.Text = "Thoát";
+            btexit.UseVisualStyleBackColor = true;
+            btexit.Click += btexit_Click;
             // 
             // btlogin
             // 
-            this.btlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btlogin.ForeColor = System.Drawing.Color.Black;
-            this.btlogin.Location = new System.Drawing.Point(488, 380);
-            this.btlogin.Name = "btlogin";
-            this.btlogin.Size = new System.Drawing.Size(157, 33);
-            this.btlogin.TabIndex = 3;
-            this.btlogin.Text = "Đăng nhập";
-            this.btlogin.UseVisualStyleBackColor = true;
-            this.btlogin.Click += new System.EventHandler(this.button1_Click);
+            btlogin.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            btlogin.ForeColor = Color.Black;
+            btlogin.Location = new Point(610, 594);
+            btlogin.Margin = new Padding(4, 5, 4, 5);
+            btlogin.Name = "btlogin";
+            btlogin.Size = new Size(196, 52);
+            btlogin.TabIndex = 3;
+            btlogin.Text = "Đăng nhập";
+            btlogin.UseVisualStyleBackColor = true;
+            btlogin.Click += button1_Click;
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.textBox1);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.ForeColor = System.Drawing.Color.White;
-            this.panel3.Location = new System.Drawing.Point(201, 288);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(610, 86);
-            this.panel3.TabIndex = 2;
-            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            panel3.BackColor = Color.Transparent;
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(label2);
+            panel3.ForeColor = Color.White;
+            panel3.Location = new Point(251, 450);
+            panel3.Margin = new Padding(4, 5, 4, 5);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(762, 134);
+            panel3.TabIndex = 2;
+            panel3.Paint += panel3_Paint;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(181, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(426, 30);
-            this.textBox1.TabIndex = 2;
+            textBox1.Location = new Point(226, 39);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(532, 35);
+            textBox1.TabIndex = 2;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 28);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 24);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Mật khẩu:";
+            label2.AutoSize = true;
+            label2.Location = new Point(5, 44);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 29);
+            label2.TabIndex = 0;
+            label2.Text = "Mật khẩu:";
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.textBoxusname);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.ForeColor = System.Drawing.Color.White;
-            this.panel2.Location = new System.Drawing.Point(201, 212);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(610, 70);
-            this.panel2.TabIndex = 0;
+            panel2.BackColor = Color.Transparent;
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.Controls.Add(textBoxusname);
+            panel2.Controls.Add(label1);
+            panel2.ForeColor = Color.White;
+            panel2.Location = new Point(251, 331);
+            panel2.Margin = new Padding(4, 5, 4, 5);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(762, 109);
+            panel2.TabIndex = 0;
             // 
             // textBoxusname
             // 
-            this.textBoxusname.Location = new System.Drawing.Point(181, 25);
-            this.textBoxusname.Name = "textBoxusname";
-            this.textBoxusname.Size = new System.Drawing.Size(426, 30);
-            this.textBoxusname.TabIndex = 1;
+            textBoxusname.Location = new Point(226, 39);
+            textBoxusname.Margin = new Padding(4, 5, 4, 5);
+            textBoxusname.Name = "textBoxusname";
+            textBoxusname.Size = new Size(532, 35);
+            textBoxusname.TabIndex = 1;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(4, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Tên đăng nhập:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(5, 44);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(193, 29);
+            label1.TabIndex = 0;
+            label1.Text = "Tên đăng nhập:";
+            label1.Click += label1_Click;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::QLST.Properties.Resources.pngtree_an_old_bookcase_in_a_library_image_2642908;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(994, 582);
-            this.Controls.Add(this.panel1);
-            this.Name = "Login";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Thư Viện Điện Tử";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Login_FormClosing);
-            this.Load += new System.EventHandler(this.Login_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1242, 909);
+            Controls.Add(panel1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Thư Viện Điện Tử";
+            FormClosing += Login_FormClosing;
+            Load += Login_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
